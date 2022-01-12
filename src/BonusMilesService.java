eec63e29e3c27b07f5a5c102533a6a673663e12b
Andrey <azenin010@gmail.com>
2022-01-12 19:34:58 +0400

    commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,7 +2,7 @@ public class BonusMilesService {
     public int calculate(int price) {
         int bonus = 20;
         int amountBonus = 1;
-        int cost = price/bonus * amountBonus;
+        int cost = price / bonus * amountBonus;
         return cost;
     }
 
